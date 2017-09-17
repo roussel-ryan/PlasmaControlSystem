f0e91d5c9028d4c42e2033e228659c58995e3ef1
scawsome <roussel@ucla.edu>
2017-09-17 15:51:44 +0300

    Added docstrings to serialDevice.py

--- ControlSystem/hardware/serial_connection.py
+++ ControlSystem/hardware/serial_connection.py
@@ -4,8 +4,36 @@ import serial
 
 
 class SerialConnection(object):
+    """
+    Creates and interfaces with a single serial device.
+    
+    Attributes:
+    -----------
+    name        = human reference name of device
+    _logger     = object specific logger object
+    _state      = enable/disable state of the device
+    _connection = pySerial object
+    
+    Methods:
+    --------
+    is_ok()     = returns enable/disable state of device
+    send(msg)   = sends msg to device
+    receive()   = reads one line of serial buffer and returns data 
+    """
 
     def __init__(self, name, port, baudrate = 9600):
+        """
+        Initialization of serial device
+        
+        Creates device object and attempts to read, writes error message if it fails
+        
+        Parameters:
+        -----------
+        name        = human reference name of device, str
+        port        = device port, int
+        baudrate    = data read rate in bytes/sec NOTE: only specific rates are allowed 
+                        and must match device baudrate, int
+        """
         self.name = name
         self._logger = logging.getLogger('SerialConnection "{}"'.format(self.name))
         self._state = True
@@ -23,9 +51,17 @@ class SerialConnection(object):
         self.receive()
 
     def is_ok(self):
+        """ returns last known state of device"""
         return self._state
 
     def send(self, message):
+        """
+        Writes a message to the serial device
+        
+        Parameters:
+        -----------
+        message     = message to be written to device, str
+        """
         if self._state:
             try:
                 self._connection.write((message + '\n').encode())
@@ -34,6 +70,13 @@ class SerialConnection(object):
                 self._state = False
 
     def receive(self):
+        """
+        Reads one line of buffer from serial device
+        
+        Returns:
+        --------
+        str     = first line of current device buffer, NoneType if it fails
+        """
         if self._state:
             time.sleep(0.1)
             try:
