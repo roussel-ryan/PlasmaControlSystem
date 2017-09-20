5afca62a40f4b4ee97d195bbedc2b76e6f0b471f
scawsome <roussel@ucla.edu>
2017-09-20 17:58:42 +0300

    Added docstrings

--- ControlSystem/hardware/device.py
+++ ControlSystem/hardware/device.py
@@ -2,8 +2,28 @@ import logging
 
 
 class Device(object):
+	"""
+	Representation of a single device in the plasma system
+	Handles device interlocks
+	
+	Attributes:
+    	-----------
+	name        	= human reference name of device
+	_logger     	= object specific logger object
+	_state      	= current enable/disable/error status of device
 
+
+    	Methods:
+    	--------
+    	lock()		= locks device
+    	unlock()   	= unlocks device
+    	set_error()   	= sets device status to error 
+	
+	"""
 	def __init__(self, name):
+		"""
+		Creates device object with name and logger
+		"""
 		self.name = name
 		self._state = 'ok'
 		self._logger = logging.getLogger('[Device "{}"]'.format(self.name))
@@ -21,6 +41,10 @@ class Device(object):
 
 	@staticmethod
 	def first_check_state(method):
+		"""
+		Decorator for device implementation which checks device status before 
+		executing commnad
+		"""
 		def wrapper(self, *args, **kwargs):
 			if self._state == 'ok':
 				return method(self, *args, **kwargs)
