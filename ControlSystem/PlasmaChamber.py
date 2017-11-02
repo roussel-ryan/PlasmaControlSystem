62b0f39a2846c0e9d761b210ac1e0064dd652738
Jasper Hansel <jasperhansel@gmail.com>
2017-11-01 17:48:45 -0700

    ready to test

--- ControlSystem/PlasmaChamber.py
+++ ControlSystem/PlasmaChamber.py
@@ -3,66 +3,18 @@ from . import QueueManager
 
 class PlasmaChamber(object):
 
-    state_variables = ('solenoid_current', 'solenoid_voltage', 'heater_current',
-       'heater_voltage','discharge_current', 'discharge_voltage',
-       'chamber_pressure', 'water_interlock', 'pressure_interlock')
-
     def __init__(self):
-        self.water_interlock = False
-        self.pressure_interlock = False
         self._queue_manager = QueueManager.QueueManager()
+        self.solenoid_current_setpoint = None
 
     def stop(self):
-        pass
+        self._queue_manager.stop()
 
     @property
     def solenoid_current(self):
-        return self._queue_manager.getState('solenoid_current')
-
-    @property
-    def solenoid_voltage(self):
-        return None
-
-    @property
-    def discharge_voltage(self):
-        return None
-
-    @property
-    def discharge_current(self):
-        return None
-
-    @property
-    def heater_current(self):
-        return None
-
-    @property
-    def heater_voltage(self):
-        return None
-
-    @property
-    def chamber_pressure(self):
-        return None
+        return self._queue_manager.get_state('solenoid_current')
 
     @solenoid_current.setter
-    def solenoid_current(self, solenoid_current):
-        pass
-
-    @solenoid_voltage.setter
-    def solenoid_voltage(self, solenoid_voltage):
-        pass
-
-    @heater_current.setter
-    def heater_current(self, heater_current):
-        pass
-
-    @heater_voltage.setter
-    def heater_voltage(self, heater_voltage):
-        pass
-
-    @discharge_current.setter
-    def discharge_current(self, discharge_current):
-        pass
-
-    @discharge_voltage.setter
-    def discharge_voltage(self, discharge_voltage):
-        pass
+    def solenoid_current(self, value):
+        self.solenoid_current_setpoint = value
+        self._queue_manager.add_command('SET_SOLENOID_CURRENT {}'.format(value))
--- ControlSystem/QueueManager.py
+++ ControlSystem/QueueManager.py
@@ -10,15 +10,14 @@ class QueueManager(object):
     def __init__(self):
         self._arduino_handler = ArduinoHandler.ArduinoHandler('/dev/cu.usbmodem1421')
         self._queue = queue.Queue()
-        self._update_timer = 0
         self._state = {
-            'solenoid_current': 0.0, 'solenoid_voltage': 0.0,
-            'heater_current': 0.0, 'heater_voltage': 0.0,
-            'discharge_current': 0.0, 'discharge_voltage': 0.0,
-            'chamber_pressure': 0.0
+            'solenoid_current': None, 'solenoid_voltage': None,
+            'heater_current': None, 'heater_voltage': None,
+            'discharge_current': None, 'discharge_voltage': None,
+            'chamber_pressure': None
         }
         self._state_lock = threading.Lock()
-        self._thread = threading.Thread(target=self.processQueue)
+        self._thread = threading.Thread(target=self.process_queue)
         self._thread.start()
 
     def stop(self):
@@ -26,42 +25,24 @@ class QueueManager(object):
         self._thread.join()
         self._arduino_handler.close()
 
-    def getState(self, name):
+    def add_command(self, command):
+        logging.debug('adding command \'{}\' to the queue (length is now {})'.format(command, self._queue.qsize()))
+        self._queue.put(command)
+
+    def get_state(self, name):
         with self._state_lock:
             return self._state[name]
 
-    def processQueue(self):
+    def process_queue(self):
         try:
             while True:
-                #if self._update_timer == 10:
-                #    self.update_timer = 0
-                #    self.addUpdateCommands()
-                #else:
-                #    self._update_timer += 1
                 command = self._queue.get()
-                logging.debug('executing command \'{}\' from queue'.format(command))
+                logging.debug('executing command \'{}\' (queue length is now {})'.format(command, self._queue.qsize()))
                 if command == 'TERMINATE':
                     return
-                self.executeCommand(command)
                 self._queue.task_done()
         except Exception as e:
-            logging.exception(e)
+            print(e)
+            import sys
+            sys.stdout.flush()
             os._exit(1)
-
-    def addUpdateCommands(self):
-        self._queue.put('GET_SOLENOID_CURRENT')
-
-    def executeCommand(self, command):
-        if command == 'GET_SOLENOID_CURRENT':
-            values = []
-            for _ in range(10):
-                value = self._arduino_handler.query('GET_SOLENOID_CURRENT')
-                values.append(value)
-            if None in values:
-                result = None
-            else:
-                result = sum(values) / len(values)
-            with self._state_lock:
-                self._state['solenoid_current'] = result
-        else:
-            assert False
